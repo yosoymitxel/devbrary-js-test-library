a4f3a9dfe8f35600bb745c10f487606791aeeed1
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2020-08-22 10:29:06 -0400

    Up dev_str_substring

--- devbrary.js
+++ devbrary.js
@@ -417,7 +417,7 @@ function dev_str_acortar_texto(t,cantidadCaracteres) {
 function dev_str_substring(t,inicio, fin) {
     if(dev_is_string(t,1) && dev_is_numero(inicio) && dev_is_numero(fin)){
         if(fin<0){
-            return t.substring(inicio, (t.length - fin) )
+            return t.substring(inicio, (t.length + fin) )
         }
         return t.substring(inicio,fin)
     }
